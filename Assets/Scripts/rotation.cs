a052f31a28c3627be3df3f4cd09e03512cd14f64
nyunyunyu <raitokaosutyao@gmail.com>
2014-05-24 10:45:51 +0900

    最初のコミットです。（テスト用）

---
+++ Assets/Scripts/rotation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotation : MonoBehaviour {
+
+    [SerializeField]
+    float velocity = 50.0f;
+
+    Transform myTransform;
+
+	// Use this for initialization
+	void Start () {
+        myTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        myTransform.Rotate(0, velocity * Time.deltaTime, 0);
+	}
+}
