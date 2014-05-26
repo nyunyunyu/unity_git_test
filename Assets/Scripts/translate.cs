c97300fc99da0cdf26244f50d6bbca6e59992c15
nyunyunyu <raitokaosutyao@gmail.com>
2014-05-26 11:57:13 +0900

    add translate

---
+++ Assets/Scripts/translate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class translate : MonoBehaviour {
+
+    [SerializeField]
+    float velocity = 5.0f;
+
+    Transform myTransform;
+
+    // Use this for initialization
+    void Start()    {
+        myTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()    {
+        myTransform.Translate(0, velocity * Time.deltaTime, 0);
+    }
+}
